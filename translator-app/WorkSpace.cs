6b1dcb15a87df1084300f26d9d7a83cd658c55db
Ali Ramazan TAŞDELEN <alitasdelen@ogrenci.karabuk.edu.tr>
2021-10-29 22:25:25 +0300

    Added Tooltip and Character Buttons

--- translator-app/WorkSpace.cs
+++ translator-app/WorkSpace.cs
@@ -5,8 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Web;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace translator_app
 {
@@ -15,10 +18,11 @@ namespace translator_app
 
         String Text;
         int p, c, n;
+        int btn_counter = 0;
         List<String> textParts = new List<String>();
+
         public WorkSpace()
         {
-
             InitializeComponent();
 
 
@@ -103,6 +107,98 @@ namespace translator_app
 
         private void button7_Click(object sender, EventArgs e)
         {
+            
+                Button b = new Button();
+                b.Text = textBox1.Text;
+                b.Size = button7.Size;
+                b.Name = "btn_" + textBox1.Text;
+                b.Location = new Point(button7.Location.X, button7.Location.Y + button7.Height * (btn_counter+1) );
+                b.Click += new EventHandler(this.characterButtonFunct);
+                Controls.Add(b);
+                btn_counter++;
+            
+
+        }
+
+        public void characterButtonFunct(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            richTextBox2.Text = button.Text + "--" + richTextBox2.Text;
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+            
+        }
+
+        ToolTip tip = new ToolTip();
+
+        private void richTextBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            string word = GetWord(richTextBox2.Text, richTextBox2.GetCharIndexFromPosition(e.Location));
+            
+            //var chr = richTextBox2.GetCharIndexFromPosition(GetMousePositionWindowsForms());
+            tip.ToolTipTitle = word;
+            Point p = richTextBox2.Location;
+            tip.Show(Translate(word), this, p.X + e.X,
+                        p.Y + e.Y + 32, //You can change it (the 35) to the tooltip's height - controls the tooltips position.
+                        1000);
+            Thread.Sleep(1000);
+        }
+
+        public static string GetWord(string input, int position) //Extracts the whole word the mouse is currently focused on.
+        {
+            char s = input[position];
+            int sp1 = 0, sp2 = input.Length;
+            for (int i = position; i > 0; i--)
+            {
+                char ch = input[i];
+                if (ch == ' ' || ch == '\n')
+                {
+                    sp1 = i;
+                    break;
+                }
+            }
+
+            for (int i = position; i < input.Length; i++)
+            {
+                char ch = input[i];
+                if (ch == ' ' || ch == '\n')
+                {
+                    sp2 = i;
+                    break;
+                }
+            }
+
+            return input.Substring(sp1, sp2 - sp1).Replace("\n", "");
+        }
+
+        public String Translate(String word)
+        {
+            var toLanguage = "tr";//English
+            var fromLanguage = "en";//Turkish
+            var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + fromLanguage + "&tl=" + toLanguage + "&dt=t&q=" + HttpUtility.UrlEncode(word);
+            var webClient = new WebClient
+            {
+                Encoding = System.Text.Encoding.UTF8
+            };
+            var result = webClient.DownloadString(url);
+            try
+            {
+                result = result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+                return result;
+            }
+            catch
+            {
+                return "Error";
+            }
+        }
+
+        public static Point GetMousePositionWindowsForms()
+        {
+            var point = Control.MousePosition;
+            return new Point(point.X, point.Y);
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -128,6 +224,15 @@ namespace translator_app
             richTextBox1.Text = textParts[p];
             richTextBox2.Text = textParts[c];
             richTextBox3.Text = textParts[n];
+
+            
+
+            int blockStart = 2; //arbitrary numbers to test
+            int blockLength = 4;
+            richTextBox1.SelectionStart = blockStart;
+            richTextBox1.SelectionLength = blockLength;
+            richTextBox1.SelectionBackColor = Color.Yellow;
+
 
         }
 
