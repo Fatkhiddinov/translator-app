98401231c0998443b00787f96f029910e341d5d8
Kamoliddin Fatkhiddinov <fatxuddinov@gmail.com>
2021-10-29 20:43:34 +0300

    Prototype v2

---
+++ translator-app/WorkSpace.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace translator_app
+{
+    partial class WorkSpace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WorkSpace));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(252, 1);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(422, 231);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Resize += new System.EventHandler(this.axWindowsMediaPlayer1_Resize);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(781, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(757, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(33, 141);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(33, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(252, 186);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(422, 46);
+            this.panel1.TabIndex = 6;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Swis721 BlkCn BT", 15.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(252, 228);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(422, 84);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(252, 318);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(422, 84);
+            this.richTextBox2.TabIndex = 8;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Location = new System.Drawing.Point(252, 408);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(422, 84);
+            this.richTextBox3.TabIndex = 9;
+            this.richTextBox3.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(45, 308);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Choose File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(204, 363);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "^";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(204, 408);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "v";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // WorkSpace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(960, 540);
+            this.ControlBox = false;
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WorkSpace";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WorkSpace";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Resize += new System.EventHandler(this.WorkSpace_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
---
+++ translator-app/WorkSpace.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace translator_app
+{
+    public partial class WorkSpace : Form
+    {
+
+        String Text;
+        int p, c, n;
+        List<String> textParts = new List<String>();
+        public WorkSpace()
+        {
+
+            InitializeComponent();
+
+
+        }
+
+        
+
+        private void axWindowsMediaPlayer1_Resize(object sender, EventArgs e)
+        {
+            
+
+
+
+
+
+        }
+
+        private void WorkSpace_Resize(object sender, EventArgs e)
+        {
+            Size formSize = new Size(this.Width/2, this.Height/2);
+            Point mediaPlayerLocation = new Point(this.Width / 4, 0);
+            axWindowsMediaPlayer1.Location = mediaPlayerLocation;
+            axWindowsMediaPlayer1.Size = formSize;
+
+            Point zeroPoint = new Point(0, 0);
+
+            richTextBox1.Location = new Point(this.Width / 4, axWindowsMediaPlayer1.Height);
+            richTextBox1.Size = new Size(axWindowsMediaPlayer1.Width, this.Height / 6);
+
+
+            richTextBox2.Location = new Point(this.Width / 4, axWindowsMediaPlayer1.Height+richTextBox1.Height);
+            richTextBox2.Size = new Size(axWindowsMediaPlayer1.Width, this.Height / 6);
+
+            richTextBox3.Location = new Point(this.Width / 4, axWindowsMediaPlayer1.Height + richTextBox1.Height + richTextBox2.Height);
+            richTextBox3.Size = new Size(axWindowsMediaPlayer1.Width, this.Height / 6);
+
+            panel1.Size = new Size(axWindowsMediaPlayer1.Width, 46);
+            panel1.Location = new Point(axWindowsMediaPlayer1.Location.X, axWindowsMediaPlayer1.Height-46);
+            panel1.BackColor = Color.White;
+
+
+
+            button1.Width = this.Width/4;
+            button1.Location= zeroPoint;
+            button1.Text = (button1.Size).ToString() ;
+
+
+            button2.Location = new Point(axWindowsMediaPlayer1.Size.Width + button1.Size.Width);
+            button2.Width = this.Width - button1.Width - axWindowsMediaPlayer1.Width;
+            button2.Text = (button2.Size).ToString();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textParts.Count() > n)
+            {
+                textParts[p] = richTextBox1.Text;
+                textParts[c] = richTextBox2.Text;
+                textParts[n] = richTextBox3.Text;
+
+                p += 4; c += 4; n += 4;
+
+
+                richTextBox1.Text = textParts[p];
+                richTextBox2.Text = textParts[c];
+                richTextBox3.Text = textParts[n];
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            
+            
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+               
+                var OpenFile = new System.IO.StreamReader(openFileDialog2.FileName);
+
+
+                string[] splittedText = File.ReadAllLines(openFileDialog2.FileName);
+
+                textParts.AddRange(splittedText);
+                Text = OpenFile.ReadToEnd();
+            }
+
+            p = 2;
+            c = 6;
+            n = 10;
+            richTextBox1.Text = textParts[p];
+            richTextBox2.Text = textParts[c];
+            richTextBox3.Text = textParts[n];
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (p > 4)
+            {
+
+                textParts[p] = richTextBox1.Text;
+                textParts[c] = richTextBox2.Text;
+                textParts[n] = richTextBox3.Text;
+
+                p -= 4; c -= 4; n -= 4;
+                richTextBox1.Text = textParts[p];
+                richTextBox2.Text = textParts[c];
+                richTextBox3.Text = textParts[n];
+
+
+            }
+
+        }
+    }
+    }
+
